b94c2822be0ecd7c0d9fc8b3e87a6549a3a743fb
adhyfa <adhyfafahmy@gmail.com>
2019-04-16 18:02:50 +0700

    commit

--- step-definitions/inputDataTransaksi.js
+++ step-definitions/inputDataTransaksi.js
@@ -15,22 +15,22 @@ module.exports = function(){
     //         return page.menuKelolaPenjualan.perform()
     //     })
     // })
-    this.Given(/^kasir masukkan total penjualan$/, function(){
-        driver.then(function(){
-            return page.fillPenjualan.perform()
-        })
-    })
-    this.Given(/^kasir masukkan total laba$/, function(){
-        driver.then(function(){
-            return page.fillLaba.perform()
-        })
-    })
-    this.When(/^kasir klik add categoryn$/, function(){
-        driver.then(function(){
-            return page.btnLogin.perform()
-        })
-    })
-    this.Then(/^data berhasil masuk$/, function(){
-        helpers.loadPage(shared.pages.page2)
-    })
+    // this.Given(/^kasir masukkan total penjualan$/, function(){
+    //     driver.then(function(){
+    //         return page.fillPenjualan.perform()
+    //     })
+    // })
+    // this.Given(/^kasir masukkan total laba$/, function(){
+    //     driver.then(function(){
+    //         return page.fillLaba.perform()
+    //     })
+    // })
+    // this.When(/^kasir klik add categoryn$/, function(){
+    //     driver.then(function(){
+    //         return page.btnLogin.perform()
+    //     })
+    // })
+    // this.Then(/^data berhasil masuk$/, function(){
+    //     helpers.loadPage(shared.pages.page2)
+    // })
 }
